b4dec8021e089cf0924b8f15620595712fe478b4
MarvinSt <m.struijk@outlook.com>
2023-04-30 13:26:56 +0200

    update documentation

--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -13,6 +13,6 @@ var searchData=
   ['createsteeringsystem_10',['createSteeringSystem',['../class_steering_system.html#a852e7eea72c293eb5097d236498149b9',1,'SteeringSystem']]],
   ['createtorsionalstiffness_11',['createTorsionalStiffness',['../class_anti_rollbar.html#a6993cee42f870894d940a77b9f152c48',1,'AntiRollbar']]],
   ['createupright_12',['createUpright',['../class_suspension_system.html#ab94281f5939630328e7b019ee4eac524',1,'SuspensionSystem']]],
-  ['createwheel_13',['createWheel',['../class_wheel.html#a14bd1dc3fdc35f797f1db73a685b6ede',1,'Wheel']]],
+  ['createwheel_13',['createWheel',['../class_wheel.html#ade497acfeb7ad19c76c5d91754f52fa0',1,'Wheel']]],
   ['createwheelcollider_14',['createWheelCollider',['../class_wheel.html#a35f87c3b275198c89c033db5eea3d756',1,'Wheel']]]
 ];
--- docs/search/functions_9.js
+++ docs/search/functions_9.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['wheel_0',['Wheel',['../class_wheel.html#a0605feaf16aaf7a2c3abd1e582396fc7',1,'Wheel']]]
+  ['wheel_0',['Wheel',['../class_wheel.html#ab455e72ed7ee58830f4dd1589147c29b',1,'Wheel']]]
 ];
